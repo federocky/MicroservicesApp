070ff76269478665dc3bcf968366e6f5fb0919e1
DESKTOP-DHNG922\Federico <fedeandresdeveloper@gmail.com>
2025-06-08 20:55:56 +0200

    Full working with event published and consumed

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,3 +1,4 @@
+using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddOpenApi();
     builder.Services.AddControllers();
+
+    builder.Services.AddHostedService<MessageBusSubscriber>();
     
     builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
     builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
